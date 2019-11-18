69ec2c0c436cce94de60b05efcc0b3d6b3b60c60
Ubuntu <ubuntu@ip-172-31-92-98.ec2.internal>
2019-11-18 01:52:09 +0000

    commit

--- config/dynamo.config.js
+++ config/dynamo.config.js
@@ -1,4 +1,11 @@
 const AWS = require('./database.config.js');
 
-const dynamodb = new AWS.DynamoDB({region: "us-east-1"});
+let credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:2f30ad67-2357-4e6b-a63f-e50b0239bae4',
+  RoleArn: "arn:aws:iam::645836090925:role/Cognito_nodeawsUnauth_Role"
+}, {
+  region: "us-east-1"
+});
+
+const dynamodb = new AWS.DynamoDB(credentials);
 module.exports = dynamodb;
